e2c31be973e51599dd9ed62255027776e752ea4e
aidenj1808 <aidenj1808@gmail.com>
2024-07-31 09:18:12 -0600

    Use llama3.1 model instead of custom and simplify the prompt.

--- src/llama3_1_agent.py
+++ src/llama3_1_agent.py
@@ -27,28 +27,17 @@ All:
 ollama pull llama3.1
 
 """
-
 import sys
 import ollama
 
 
-MODELFILE = """
-FROM llama3.1
-SYSTEM You are a judge for the card game Apples to Apples. Given a hand of nouns that are red cards, determine the noun with the most association with the adjective which is the green card. Your output should only contain the one noun card and nothing else. If you can't say one then choose one. Your output should only be the word you choose and nothing else. Your output should not contain any explanation and just the single word or phrase.
-"""
-
-model_created = False
-for model in ollama.list()["models"]:
-    if model["name"] == "judge":
-        model_created = True
-
-if not model_created:
-    ollama.create(model="judge", modelfile=MODELFILE) 
-
 def agent_function(hand: list[str], green_card: str) -> str:
-    prompt = f"Noun red cards: {str(hand)[1: -1]}, Adjective green card: {green_card}"
-    output = ollama.generate(model="judge", prompt=prompt)
-    return output["response"]
+    hand = [card.lower() for card in hand]
+    prompt = (f"What noun is most associated with {green_card}? {hand}."
+        "Give me just your answer as one word and no explanation or punctuation.")
+    output = ollama.generate(model="llama3.1", prompt=prompt)["response"]
+    return output
+
     
 def main():
     hand = sys.argv[1].split(',')
