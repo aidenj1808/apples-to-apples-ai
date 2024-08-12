f1f58db1d27416873d980525a5cc75564c6b3d74
Malcolm Nielsen <87519451+notMarth@users.noreply.github.com>
2024-08-12 14:15:04 -0600

    Fix rebase changes

--- src/game/deck.py
+++ src/game/deck.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 
 class Deck():
@@ -20,26 +19,3 @@ class Deck():
     def draw_card(self):
         self.size -= 1
         return self.cards.pop()
-=======
-import random
-
-class Deck():
-    def __init__(self, filename):
-        self.cards = []
-        self.size = 0
-        self.filename = filename
-
-        with open(filename, 'r') as file:
-            file.readline()
-            for line in file:
-                _, card, _ = line.strip().split(",", 2)
-                self.cards.append(card)
-                self.size += 1
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw_card(self):
-        self.size -= 1
-        return self.cards.pop(0)
->>>>>>> 88cbc23 (Commit interim changes)
