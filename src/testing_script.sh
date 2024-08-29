4acefb29a04dda639299466b83de3ab82fc1a8b5
aidenj1808 <aidenj1808@gmail.com>
2024-08-28 21:13:53 -0600

    Add testing game results for 30 games.

--- src/testing_script.sh
+++ src/testing_script.sh
@@ -3,16 +3,28 @@
 #USAGE: bash testing_script NUM_GAMES
 
 cd 10
+echo "-------------------------------------------------------------------------"
+echo "                      TESTING 10 Cluster Games                           "
+echo "-------------------------------------------------------------------------"
 python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
 
 cd ..
 cd 25
+echo "-------------------------------------------------------------------------"
+echo "                      TESTING 25 Cluster Games                           "
+echo "-------------------------------------------------------------------------"
 python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
 
 cd ..
 cd 50
+echo "-------------------------------------------------------------------------"
+echo "                      TESTING 50 Cluster Games                           "
+echo "-------------------------------------------------------------------------"
 python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
 
 cd ..
 cd 100
+echo "-------------------------------------------------------------------------"
+echo "                      TESTING 100 Cluster Games                          "
+echo "-------------------------------------------------------------------------"
 python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
