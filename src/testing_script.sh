c080577103d77fa2a5cdf26d4cfbfa7aa6cb5f89
aidenj1808 <aidenj1808@gmail.com>
2024-09-02 14:18:12 -0600

    Change testing script to run 65 clusters agent with updating policy after each game.

--- src/testing_script.sh
+++ src/testing_script.sh
@@ -3,21 +3,11 @@
 #USAGE: bash testing_script NUM_GAMES
 
 cd 65
-echo "-------------------------------------------------------------------------"
-echo "                      TESTING 65 Cluster Games                           "
-echo "-------------------------------------------------------------------------"
-python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
-
-cd ..
-cd 70
-echo "-------------------------------------------------------------------------"
-echo "                      TESTING 70 Cluster Games                           "
-echo "-------------------------------------------------------------------------"
-python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
-
-cd ..
-cd 75
-echo "-------------------------------------------------------------------------"
-echo "                      TESTING 75 Cluster Games                           "
-echo "-------------------------------------------------------------------------"
-python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py $1
+for i in $(seq 1 $1)
+do
+    echo "---------------------------------------------------------------------"
+    echo "                TESTING 65 Clusters Agent                            "
+    echo "                        Game: $i                                     "
+    echo "---------------------------------------------------------------------"
+    python3 driver_async_rl.py llama_agent.py gemma_agent.py sbert_agent.py RLAgent.py 1
+done
